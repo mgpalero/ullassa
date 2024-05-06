8cc1372b0fcd16fdf99a3303dd7c384d8ab44f48
mgpalero <157506335+mgpalero@users.noreply.github.com>
2024-05-06 20:41:21 -0300

    Add files via upload

---
+++ js/scipt.js
@@ -0,0 +1,8 @@
+
+let ingresanomnre = prompt("nombre");
+let ingresaapellido = prompt ("apellido")
+if ((ingresanomnre !="") && (apellido !="")) {
+    alert("Nombre: " + ingresanomnre + "\nApellido: " + ingresaapellido);
+} else { 
+    alert("Error: Ingresa datos");
+}
